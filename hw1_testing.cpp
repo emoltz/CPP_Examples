cc4dae6c9618fc1d44aaef66e468aaf5833c19d2
emoltz <es5541@nyu.edu>
2022-01-25 16:15:30 -0500

    2

--- hw1_testing.cpp
+++ hw1_testing.cpp
@@ -1,4 +1,16 @@
-//
-// Created by Ethan Shafran Moltz on 1/23/22.
-//
-
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main() {
+    int num1;
+    int num2;
+    cout << "Please enter two positive integers, separated by a space:";
+    cin >> num1 >> num2;
+    cout << num1 << " + " << num2 << " = " << (num1+num2) << endl;
+    cout << num1 << " - " << num2 << " = " << (num1-num2) << endl;
+    cout << num1 << " * " << num2 << " = " << (num1*num2) << endl;
+    cout << num1 << " / " << num2 << " = " << (num1/num2) << endl;
+    div_t result1 = div(num1, num2);
+    cout << num1 << " div " << num2 << " = " << result1.quot<< endl;
+    cout << num1 << " mod " << num2 << " = " << (num1%num2) << endl;
+}
