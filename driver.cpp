ca2d55f25c21689115026d53a55e7a3a0b071434
emoltz <es5541@nyu.edu>
2022-03-18 21:41:41 -0400

    finished HW9

---
+++ driver.cpp
@@ -0,0 +1,2 @@
+const int arrSize = 10;
+int arr[arrSize] = { 5, -2, -1, 0, 1, 2, 3, -4, 5, 6 };
--- studentWork.cpp
+++ studentWork.cpp
@@ -1,26 +1,7 @@
-//hw11 q4 recursive LowestTravelCost
 #include <iostream>
 using namespace std;
-int LowestTravelCost (int arr [], int arrSize){
-  int temp;
-  if (arrSize == 3) {
-    return arr[arrSize-1]; //Base case is {0, X, Y} and cost is Y
-    }
-  else {
-    if ((arr[0] + arr[2]) < arr[1]) {
-      temp = (arr[2]);
-      return temp + LowestTravelCost (arr+1, arrSize-1);
-      }
-    else {
-      temp = arr[1];
-      return temp + LowestTravelCost (arr+1, arrSize-1);
-      }
-    }
-}
-int main() {
-  int arrSize = 6;
-  int arr[6] = {0, 3, 6, 80, 57, 10}; //should give 73
-  int result = LowestTravelCost(arr, arrSize);
-  cout << result << endl;
-  return 0;
+
+int main(){
+    const int n = 5;
+    int* array = new int[n]{1, 2, 3, 4, 5};
 }
