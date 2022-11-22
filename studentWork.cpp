13ad66274f2cbf5a160732f089b6f47a09c39a8e
emoltz <es5541@nyu.edu>
2022-11-21 19:27:25 -0500

    new examples

--- studentWork.cpp
+++ studentWork.cpp
@@ -1,84 +1,68 @@
-#include<iostream>
-#include<vector>
+#include <list>
+#include <iostream>
+#include <stack>
+#include <queue>
+#include <vector>
+
 using namespace std;
 
-void getUserInput(vector<string> &outputVector);
-void removeSymbols(vector<string> userInput, vector<string> &outputArray, int &allSymbol, int &noSymbol);
 
-int main(){
-
-    vector<string> userInputs;
-    vector<string> cleanStrings;
-    vector<string> charString;
-    int allSymbol = 0;
-    int noSymbol = 0;
-
-    getUserInput(userInputs);
-
-    removeSymbols(userInputs, cleanStrings, allSymbol, noSymbol);
-
-    cout<<endl;
-    for (int i = 0; i<cleanStrings.size(); i++){
-        cout<<cleanStrings[i]<<endl;
+vector<int> strangeSort(vector<int>& V)
+{
+    int size = V.size();
+    vector<int> even;
+    vector<int> odd;
+    if (V.size ==0)
+    {
+        return V;
     }
-    cout<<"Number of Strings in the input sequence that contain only special\ncharacters: "<<allSymbol<<endl;
-    cout<<"Number of Strings in the input sequence that contain only lowercase\nEnglish letters: "<<noSymbol<<endl;
-
-
-    return 0;
-}
-
-void getUserInput(vector<string> &outputVector){
-
-    string tempString;
-
-    cout<<"Please enter a non-empty sequence of Strings. Each string should\nbe in a separate line and consists of only lowercase English\nletters and/or special characters @, #, $, !, &. To indicate the\nend of the input, enter a -1.\n";
-    do{
-        cin>>tempString;
-        outputVector.push_back(tempString);
-
-    } while (tempString != " ");
-    outputVector.pop_back();
-
-}
-
-void removeSymbols(vector<string> userInput, vector<string> &outputArray, int &allSymbol, int &noSymbol){
-
-    vector<int> lineCheck;
-    string tempString = "";
-    int tempLen;
-    string newString;
-    int symbolCount;
-
-    for (int i = 0; i < userInput.size(); i++){
-        tempString = userInput[i];
-        newString = "";
-        symbolCount = 0;
-        for (int i = 0; i < tempString.length(); i++){
-            if (tempString[i] >= 'a' && tempString[i] <= 'z'){
-                lineCheck.push_back(1);
-            } else {
-                lineCheck.push_back(0);
+    else
+    {
+        for (i =0; i<size; i++)
+        {
+            if (V[i]%2==1)
+            {
+                odd.push_back(V[i]);
+            }
+            else //if (V[i]%2!=1)
+            {
+                even.push_back;
             }
         }
-        for (int i = 0; i < lineCheck.size(); i++){
-            if (lineCheck[i] == 1){
-                newString += tempString[i];
-            } else {
-                symbolCount++;
-            }
+        int evenSize = even.size();
+        int oddSize = odd.size();
+        for (int i =0;i<evenSize; i++)
+        {
+            V[i]=even[i];
         }
-        lineCheck.clear();
-        if (symbolCount < tempString.length()){
-            outputArray.push_back(newString);
-        } else {
-            allSymbol++;
+        for (int i = oddSize; i <=0; i--)
+        {
+            V[evenSize]= odd[i];
+            evenSize++;
         }
-        if (symbolCount == 0){
-            noSymbol++;
-        }
+        return V;
     }
+}
 
+void printVec(vector<int> &vec) {
+    for (int i: vec) {
+        cout << i << " ";
+    }
+    cout << endl;
+}
 
+int main() {
+    vector<int> vec1{1, 2, 3, 4};
+    vector<int> vec2{3, 5, 7, 6, -2, 8};
+    vector<int> vec3{7, 2, 3, 4, 9};
+    vector<int> vec4{1, 7, 3, 4, 2, 8};
+    vector<int> vec5{2, 4};
+    vector<int> vec6;
+    strangeSort(vec3);
+
+    for (int i = 0; i < vec3.size(); i++) {
+        cout << vec3[i] << " ";
+
+    }
 
 }
